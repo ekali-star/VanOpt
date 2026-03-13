7d35721cba4d2a0a0ee156cf61db413f6dc82462
ekali-star <ekali23@freeuni.edu.ge>
2026-03-13 15:34:13 +0400

    initial commit

---
+++ src/test/java/org/example/vanopt/VanOptApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vanopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanOptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
